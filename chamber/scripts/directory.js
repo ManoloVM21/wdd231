50989fbea3861ecd90842c75d743f9582dfbe991
ManoloVM <mvasquezmendoza@byupathway.edu>
2025-05-14 20:53:47 -0700

    Chamber page ended

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,67 @@
+//HAMBUTTON
+const nav = document.querySelector("#animateme");
+const hambbutton = document.querySelector("#hamburger");
+
+hambbutton.addEventListener('click',()=>{
+    nav.classList.toggle('open');
+    hambbutton.classList.toggle('open');
+
+});
+//FOOTER
+const year = document.querySelector("#currentyear")
+const modified = document.querySelector("#lastModified");
+const today = new Date(document.lastModified);
+
+year.innerHTML = today.getFullYear();
+modified.innerHTML = `Last Update: <span id="currentyear">${new Intl.DateTimeFormat(
+    'en-US', {
+        dateStyle: 'medium',
+        timeStyle: "medium"
+    }).format(today)}</span>`;
+
+//JSON TO CARDS
+const cards = document.querySelector("#cards");
+async function getCompaniesData() {
+    const response = await fetch('data/members.json');
+    const data = await response.json();
+    displayCompanies(data);
+}
+getCompaniesData();
+
+const displayCompanies = (companies) =>{
+    companies.forEach((company) =>{
+        let website = document.createElement('a');
+        let card = document.createElement('section');
+        let name = document.createElement('h3');
+        let image = document.createElement('img');
+        let address = document.createElement('p');
+        name.textContent = `${company.name}`;
+        image.setAttribute('src',`${company.image}`);
+        image.setAttribute('alt',`${company.name}`);
+        image.setAttribute('width',"300");
+        image.setAttribute('height', "250");
+        image.setAttribute('loading',"lazy");
+        website.setAttribute('href',`${company.website}`)
+        website.setAttribute('class','website');
+        address.textContent = `${company.address}`;
+        website.textContent = `Details`;
+        card.appendChild(image);
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(website);
+        cards.appendChild(card);
+    })
+}
+
+//GRID OR LIST
+const gridbutton = document.querySelector("#gridbutton");
+const listbutton = document.querySelector("#listbutton")
+
+gridbutton.addEventListener('click', ()=>{
+    cards.classList.add("grid"),
+    cards.classList.remove("list");
+});
+listbutton.addEventListener("click",()=>{
+    cards.classList.add("list"),
+    cards.classList.remove("grid")
+})
