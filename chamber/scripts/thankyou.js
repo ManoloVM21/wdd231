28079cd415e08a7091d49be2de6d51007caa59e0
ManoloVM <mvasquezmendoza@byupathway.edu>
2025-05-29 09:24:44 -0700

    updates

--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -15,4 +15,4 @@ const membershipLevel = params.get("membership");
 const description = params.get("description");
 
 const formInfo = document.querySelector("#formInfo");
-formInfo.innerHTML = `We have received the information for ${businessName} with a ${membershipLevel} by ${name} ${lastName} who is the ${orgTitle}. We will send you more details to ${email}.Thank you for you interest! We are glad you've chosen us.`
+formInfo.innerHTML = `We have received the information for ${businessName} with a ${membershipLevel} by ${name} ${lastName} who is the ${orgTitle}. We will send you more details to ${email}.`
